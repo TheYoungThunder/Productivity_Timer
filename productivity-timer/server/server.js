49d42063fc83b79c33f830f3a9e2b3daa6eb0c2a
Bachar Hafez <expc7c19@gmail.com>
2022-04-12 03:54:23 +0300

    added function to detect user closing app

--- productivity-timer/server/server.js
+++ productivity-timer/server/server.js
@@ -1,4 +1,5 @@
 import { WebSocketServer } from "ws";
+//import * as fs from "fs";
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -9,8 +10,14 @@ wss.on("connection", function connection(ws) {
     users.forEach((user) => {
       user.send(data.toString());
     });
-    console.log(data.toString());
   });
-
   console.log("Someone new joined");
 });
+
+setInterval(() => {
+  for (let i = 0; i < users.length; i++) {
+    // splice (index of start, how many elements to delete after the start)
+    users.splice(1, 1);
+  }
+  console.log("There are", users.length, "users online");
+}, 10000);
